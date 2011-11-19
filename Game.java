c9047e8445f028aa756ffa90551af76e55a9fef5
Suchit Puri <suchitp@thoughtworks.com>
2011-11-20 00:45:35 +0530

    reverting my changes

--- Game.java
+++ Game.java
@@ -13,15 +13,15 @@ public class Game {
 		for (int i = 0; i < 9; i++) {
 			if (board.charAt(i) == '-') {
 				Game game = play(i, player);
-				if (game.winner() == player) 
+				if (game.winner() == player)
 					return i;
 			}
 		}
 
 		for (int i = 0; i < 9; i++) {
-			if (board.charAt(i) == '-') 
+			if (board.charAt(i) == '-')
 				return i;
-		}	
+		}
 		return -1;
 	}
 
@@ -30,16 +30,16 @@ public class Game {
 	}
 
 	public char winner() {
-		if (board.charAt(0) != '-' 
-            && board.charAt(0) == board.charAt(1) 
+		if (board.charAt(0) != '-'
+            && board.charAt(0) == board.charAt(1)
             && board.charAt(1) == board.charAt(2))
 			return board.charAt(0);
-		if (board.charAt(3) != '-' 
-            && board.charAt(3) == board.charAt(4) 
+		if (board.charAt(3) != '-'
+            && board.charAt(3) == board.charAt(4)
             && board.charAt(4) == board.charAt(5))
 			return board.charAt(3);
-		if (board.charAt(6) != '-' 
-            && board.charAt(6) == board.charAt(7) 
+		if (board.charAt(6) != '-'
+            && board.charAt(6) == board.charAt(7)
             && board.charAt(7) == board.charAt(8))
 			return board.charAt(6);
 		return '-';
